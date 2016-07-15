f9e9503cf205d893eb96d01420908df850511320
Luke Horwell <luke@ubuntu-mate.org>
2016-07-15 15:11:46 +0100

    Update CI build script to install dependencies.
    Despite using de.archive.ubuntu.com:
    - "python3-libtorrent" is missing.
    - "python3-wget" has no package for trusty.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install python3-libtorrent python3-wget
+# python3-libtorrent
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ trusty universe" -y
+sudo apt-get update
+sudo apt-get install python3-libtorrent -y
+
+# python3-wget (has no package for trusty)
+wget http://de.archive.ubuntu.com/ubuntu/pool/universe/p/python-wget/python3-wget_2.2-1_all.deb
+sudo dpkg -i python3-*.deb
+
 scripts/update-download-page.py --update-all
 nikola build
