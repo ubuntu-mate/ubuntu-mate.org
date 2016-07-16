c5ea4aabb469346db8fcf0f8946dd009a7df94e5
Martin Wimpress <code@flexion.org>
2016-07-16 19:14:12 +0100

    Future proof build.sh.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+CODENAME=$(lsb_release -cs)
+
 # python3-libtorrent
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ trusty universe" -y
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ ${CODENAME} universe" -y
 sudo apt-get update
 sudo apt-get install python3-libtorrent -y
 
 # python3-wget (has no package for trusty)
-wget http://de.archive.ubuntu.com/ubuntu/pool/universe/p/python-wget/python3-wget_2.2-1_all.deb
-sudo dpkg -i python3-*.deb
+if [ "${CODENAME}" == "trusty" ]; then
+    wget http://de.archive.ubuntu.com/ubuntu/pool/universe/p/python-wget/python3-wget_2.2-1_all.deb
+    sudo dpkg -i python3-*.deb
+fi
 
 scripts/update-download-page.py --update-all
 nikola build
