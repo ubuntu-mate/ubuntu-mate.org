b9970d1bccf02c3ca47fc525b95b10744b558065
Luke Horwell <luke@ubuntu-mate.org>
2017-08-22 23:46:56 +0100

    Add script for clearing CDN cache.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-$(dirname $0)/pre-deploy-patches.sh
+scripts_path=$(dirname $0)
+
+# Run any patches before deployment
+$scripts_path/pre-deploy-patches.sh
+
+# Upload to web server
 nikola deploy
+
+# Clear the CDN cache
+echo "Clearing CDN cache..."
+chmod +x $scripts_path/CDN_purge.sh
+$scripts_path/CDN_purge.sh
 
