2ac512d3f76e47285e86c48aa755c1cf9c3a84e4
Luke Horwell <luke@ubuntu-mate.org>
2017-09-02 08:15:05 +0100

    Remove commented out (deprecated) build commands.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-CODENAME=$(lsb_release -cs)
-
-# python3-libtorrent
-#sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu/ ${CODENAME} universe" -y
-#sudo apt-get update
-#sudo apt-get install python3-libtorrent -y
-
-# python3-wget (has no package for trusty)
-#if [ "${CODENAME}" == "trusty" ]; then
-#    sudo apt-get -y install python-urllib3
-#    wget http://de.archive.ubuntu.com/ubuntu/pool/universe/p/python-wget/python3-wget_2.2-1_all.deb
-#    sudo dpkg -i python3-*.deb
-#else
-#    sudo apt-get -y install python3-wget
-#fi
-
-#scripts/update-download-page.py --update-all
 npm install -g less-loader
 nikola build
 
