4f19164b919bf0d7484cfd2839da955bfd52ef1a
Martin Wimpress <code@flexion.org>
2019-04-08 20:16:54 +0100

    Update images links.

--- scripts/linkimages.sh
+++ scripts/linkimages.sh
@@ -18,12 +18,21 @@ function link_image() {
     commands+=" && ln -vsf ${TOR_PATH} /home/matey/ubuntu-mate.org/$PAGE/$TOR_FILE"
 }
 
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/ubuntu-18.04.2-beta2-desktop-armhf+raspi-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/xubuntu-18.04.2-beta2-desktop-armhf+raspi-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/arm64/ubuntu-mate-18.04.2-beta2-desktop-arm64+raspi3-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/ubuntu-mate-18.04.2-beta2-desktop-armhf+raspi-ext4.img.xz"
 link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/arm64/ubuntu-mate-18.04.2-beta1-desktop-arm64+raspi3-ext4.img.xz"
 link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/ubuntu-mate-18.04.2-beta1-desktop-armhf+raspi-ext4.img.xz"
-link_image gpd-pocket "/home/matey/ISO-Mirror/cosmic/amd64/ubuntu-mate-18.10-desktop-amd64-gpd-pocket.iso"
 link_image raspberry-pi "/home/matey/ISO-Mirror/xenial/armhf/ubuntu-mate-16.04.2-desktop-armhf-raspberry-pi.img.xz"
+link_image gpd-pocket "/home/matey/ISO-Mirror/bionic/amd64/ubuntu-mate-18.04.2-desktop-amd64-gpd-pocket.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/bionic/amd64/ubuntu-mate-18.04.2-desktop-amd64-gpd-pocket2.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/cosmic/amd64/ubuntu-mate-18.10-desktop-amd64-gpd-pocket.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/cosmic/amd64/ubuntu-mate-18.10-desktop-amd64-gpd-pocket2.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/disco/amd64/ubuntu-mate-19.04-beta-desktop-amd64-gpd-pocket.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/disco/amd64/ubuntu-mate-19.04-beta-desktop-amd64-gpd-pocket2.iso"
 
 echo "Symlinking images..."
-for region in "man" "yor"; do
+for region in man yor; do
     ssh -o StrictHostKeyChecking=no matey@$region.ubuntu-mate.net "$commands"
 done
