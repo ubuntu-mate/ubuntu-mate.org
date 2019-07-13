a979d74eb10798bf2054c3bb8e85db425bc55b7c
Luke Horwell <code@horwell.me>
2019-07-13 12:43:06 +0100

    Update Travis CI.
    + Points to preview.ubuntu-mate.org.
    + Reinstates link-images.sh script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+echo "Setting permissions..."
+find _site/ -type d -exec chmod 755 {} \;
+find _site/ -type f -exec chmod 644 {} \;
+
+echo "Syncing to server..."
+rsync -a -e "ssh -o StrictHostKeyChecking=no" --delete _site/ matey@man.ubuntu-mate.net:preview.ubuntu-mate.org/
+rsync -a -e "ssh -o StrictHostKeyChecking=no" --delete _site/ matey@yor.ubuntu-mate.net:preview.ubuntu-mate.org/
+
+echo "Clearing CDN cache..."
+chmod +x scripts/.CDN_purge.sh
+scripts/.CDN_purge.sh
---
+++ scripts/link-images.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Creates symlinks on the server linking to the actual files.
+# Commands are loaded up then executed in all in one go for each region server.
+#
+
+commands="echo OK"
+
+function link_image() {
+    local PAGE="${1}"
+    local ISO_PATH="${2}"
+    local ISO_FILE=$(basename ${ISO_PATH})
+    local TOR_PATH="${ISO_PATH}.torrent"
+    local TOR_FILE=$(basename ${TOR_PATH})
+
+    commands+=" && mkdir -p /home/matey/preview.ubuntu-mate.org/$PAGE"
+    commands+=" && ln -vsf ${ISO_PATH} /home/matey/preview.ubuntu-mate.org/$PAGE/$ISO_FILE"
+    commands+=" && ln -vsf ${TOR_PATH} /home/matey/preview.ubuntu-mate.org/$PAGE/$TOR_FILE"
+}
+
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/ubuntu-18.04.2-beta2-desktop-armhf+raspi-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/xubuntu-18.04.2-beta2-desktop-armhf+raspi-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/arm64/ubuntu-mate-18.04.2-beta2-desktop-arm64+raspi3-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/ubuntu-mate-18.04.2-beta2-desktop-armhf+raspi-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/arm64/ubuntu-mate-18.04.2-beta1-desktop-arm64+raspi3-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/bionic/armhf/ubuntu-mate-18.04.2-beta1-desktop-armhf+raspi-ext4.img.xz"
+link_image raspberry-pi "/home/matey/ISO-Mirror/xenial/armhf/ubuntu-mate-16.04.2-desktop-armhf-raspberry-pi.img.xz"
+link_image gpd-pocket "/home/matey/ISO-Mirror/bionic/amd64/ubuntu-mate-18.04.2-desktop-amd64-gpd-pocket.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/bionic/amd64/ubuntu-mate-18.04.2-desktop-amd64-gpd-pocket2.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/cosmic/amd64/ubuntu-mate-18.10-desktop-amd64-gpd-pocket.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/cosmic/amd64/ubuntu-mate-18.10-desktop-amd64-gpd-pocket2.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/disco/amd64/ubuntu-mate-19.04-beta-desktop-amd64-gpd-pocket.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/disco/amd64/ubuntu-mate-19.04-beta-desktop-amd64-gpd-pocket2.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/disco/amd64/ubuntu-mate-19.04-desktop-amd64-gpd-pocket.iso"
+link_image gpd-pocket "/home/matey/ISO-Mirror/disco/amd64/ubuntu-mate-19.04-desktop-amd64-gpd-pocket2.iso"
+
+echo "Symlinking images..."
+for region in man yor; do
+    ssh -o StrictHostKeyChecking=no matey@$region.ubuntu-mate.net "$commands"
+done
