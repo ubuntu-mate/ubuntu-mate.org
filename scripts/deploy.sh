e45f82371e1f90913ba85ec12490eb2f3308d28c
Luke Horwell <code@horwell.me>
2020-04-03 22:17:47 +0100

    CI: Only link-images once.
    Previous commit: 123aabb1f933f9cba586826cfbf2feb59ee0aa5b

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-scripts/link-images.sh
-
 echo "Setting permissions..."
 find _site/ -type d -exec chmod 755 {} \;
 find _site/ -type f -exec chmod 644 {} \;
@@ -9,6 +7,9 @@ find _site/ -type f -exec chmod 644 {} \;
 echo "Syncing to server..."
 rsync -a -e "ssh -o StrictHostKeyChecking=no" --delete _site/ matey@man.ubuntu-mate.net:ubuntu-mate.org/
 rsync -a -e "ssh -o StrictHostKeyChecking=no" --delete _site/ matey@yor.ubuntu-mate.net:ubuntu-mate.org/
+
+echo "Creating links for downloadable images..."
+scripts/link-images.sh
 
 echo "Clearing CDN cache..."
 ssh -o StrictHostKeyChecking=no matey@yor.ubuntu-mate.net /home/matey/post-deploy-actions.sh "ubuntu-mate.org"
