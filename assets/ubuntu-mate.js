c5c6abd159ee93741b1329048bd599f1c6a7e0fc
Luke Horwell <code@horwell.me>
2021-01-31 19:00:35 +0000

    Update jQuery dependency to 3.5.1

--- assets/ubuntu-mate.js
+++ assets/ubuntu-mate.js
@@ -3,7 +3,7 @@
 ---
 
 {% include js/modernizr-webp-detection.js %}
-{% include js/jquery-3.4.1.min.js %}
+{% include js/jquery-3.5.1.min.js %}
 {% include js/simple-jekyll-search.min.js %}
 {% include js/slick.min.js %}
 {% include js/ubuntu-mate.js %}
