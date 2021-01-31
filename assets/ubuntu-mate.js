da8de71ede0e4b2c95e754ede0230d7107a6af36
Luke Horwell <code@horwell.me>
2021-01-31 22:25:49 +0000

    Drop WebP detection and .webp style tags
    Since the server is sending WebP files in place for compatible clients,
    the markup doesn't need this code anymore.
    
    This prevents the website looking broken locally since the WebP files
    are not generated for banners. They'd only disappear anyway when
    watching for changes now that jekyll-webp is no more.
    
    Related: db2d513724ad6f74aa7f21b27e6b3dabec8d0f68

--- assets/ubuntu-mate.js
+++ assets/ubuntu-mate.js
@@ -2,7 +2,6 @@
 # JS for the main website.
 ---
 
-{% include js/modernizr-webp-detection.js %}
 {% include js/jquery-3.5.1.min.js %}
 {% include js/simple-jekyll-search.min.js %}
 {% include js/slick.min.js %}
