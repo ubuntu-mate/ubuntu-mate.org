386521d4f07d3977f4970267ac90b5f09f619ec8
Luke Horwell <code@horwell.me>
2024-12-09 21:42:40 +0000

    CI: Update parameters for proofing checks
    For the newer version of 'html-proofer'

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,19 +5,14 @@
 # 'html-proofer' RubyGem provided by:
 # https://github.com/gjtorikian/html-proofer
 #
-# This script contains two tests:
-#
-# <no parameters>   Check internal links, images and HTML output.
-# --external        All of the above, plus external links.
+# Optionally, run this script with '--external' to check external (remote) links.
 #
 
 cd "$(dirname $0)/../"
 
-# CI only: htmlproofer is installed in vendor folder
-if [ -d "vendor/" ]; then
-    htmlproofer="bundle exec htmlproofer"
-else
-    htmlproofer="htmlproofer"
+if [ "$(bundle exec htmlproofer --version)" == "" ]; then
+    echo "htmlproofer is not installed. Run 'bundle install' or 'gem install html-proofer."
+    exit 1
 fi
 
 function check_for_error() {
@@ -26,49 +21,37 @@ function check_for_error() {
     fi
 }
 
-if [ "$1" == "--external" ]; then
-    # i18n pages are ignored as it will take much longer and duplicate URL checks.
-    ignore_list=""
+
+# External URL checks optional
+external_check=""
+ignore_list=""
+
+if [ ! "$1" == "--external" ]; then
+    external_check="--disable-external"
+else
+    external_check="--log-level :debug"
+
+    # Ignore i18n pages
+    ignore_list="/_site/en/,"
     for lang in $(cat _i18n/locales.txt); do
         ignore_list+="/_site/$lang/,"
     done
-    ignore_list+="/_site/en/"
-
-    echo "Proofing HTML (and external links)..."
-    $htmlproofer \
-        --assume-extension \
-        --check-html \
-        --check-img-http \
-        --empty_alt_ignore \
-        --url-ignore "/ubuntu-mate.community/" \
-        --http-status-ignore "301,302,429" \
-        --file-ignore "$ignore_list" \
-        ./_site
-else
-    echo "Proofing HTML..."
-    $htmlproofer \
-        --assume-extension \
-        --check-html \
-        --check-img-http \
-        --empty_alt_ignore \
-        --disable-external \
-        ./_site
 fi
+
+echo "Proofing HTML..."
+bundle exec htmlproofer \
+    --ignore-files "$ignore_list" \
+    --no-enforce-https \
+    --allow-missing-href \
+    --ignore-empty-alt \
+    --ignore-missing-alt \
+    $external_check \
+    --only-4xx \
+    ./_site
 check_for_error $?
 
-#
-# Included checks:
-# ============================================================================
-#   --empty_alt_ignore      Mainly icons in layout/includes. More work needed
-#                           to catch every single alt tag.
-#   --internal_domains      Ubuntu MATE Community will rate limit requests.
-#
-# Excluded checks:
-# ============================================================================
-#   --check-favicon         5xx errors are for web server software.
-#                           No file would be present.
-#   --enforce-https         cdimage.ubuntu.com is only HTTP.
-#
+# Usage:
+#   --no-enforce-https      cdimage.ubuntu.com is only HTTP.
 
 # Markdown frontmatter consistency check
 ./scripts/helpers/validate-frontmatter.py
